cbcfc46eec500a8ce4f8d781b8546219039c85fd
jaynieles <brainlab.support1@ust.edu.ph>
2026-07-24 01:18:36 +0800

    Added medical forms tab with contents
    Signed-off-by: jaynieles <brainlab.support1@ust.edu.ph>

--- src/neurocare_plus/views.py
+++ src/neurocare_plus/views.py
@@ -1,5 +1,6 @@
 import view_elements
 import dearpygui.dearpygui as dpg
+from datetime import datetime
 
 from widgets import WidgetManager
 
@@ -100,6 +101,7 @@ class MainView:
         self.monitor_tab = MonitorTab()
         self.eeg_tab = EEGTab()
         self.smartwatch_tab = SmartwatchTab()
+        self.medicalforms_tab = MedicalFormsTab()
 
         dpg.show_metrics()
     
@@ -120,8 +122,7 @@ class MainView:
                 with dpg.child_window(label="Multipanel", border=False, height=0):
                     with dpg.tab_bar():
                         self.monitor_tab.build()
-                        self.eeg_tab.build()
-                        self.smartwatch_tab.build()
+                        self.medicalforms_tab.build()
             
             # Logger Panel
             self.logger_panel.build()
@@ -257,6 +258,361 @@ class MonitorTab:
 
             # Bind the theme
             dpg.bind_item_theme("monitor_table", "table_no_pad_theme")
+
+
+class MedicalFormsTab:
+    def __init__(self):
+        self.tag = "medform"
+
+        self._get_date_today()
+
+    def build(self):
+        with dpg.tab(label="Medical Forms"):
+            with dpg.child_window(height=0):
+                #----- New, Edit, Save Buttons -----
+                with dpg.group(horizontal=True):
+                    dpg.add_button(label="New", width=65, height=30, tag=f"{self.tag}_new_btn")
+                    dpg.add_button(label="Edit", width=65, height=30, tag=f"{self.tag}_edit_btn")
+                    dpg.add_button(label="Save", width=65, height=30, tag=f"{self.tag}_save_btn")
+
+                dpg.add_spacer(height=10)
+
+                #----- Search Bar -----
+                with dpg.group(horizontal=True):
+                    dpg.add_input_text(hint="Participant Code", width=300)
+                    dpg.add_button(label="Search", width=60, tag=f"{self.tag}_search_btn")
+
+                #----- Spacers for Search Bar & Forms -----
+                dpg.add_spacer(height=10)
+                dpg.add_separator()
+                dpg.add_spacer(height=2)
+
+                # ----- Start of Medical Forms -----
+                with dpg.child_window(border=False):  
+                    # First Line     
+                    with dpg.group(horizontal=True):
+                        dpg.add_text("Participant Code: ")
+                        dpg.add_input_text(width=80)
+
+                        dpg.add_spacer(width=10)
+
+                        dpg.add_text("Date of Enrollment: ")
+                        dpg.add_input_text(
+                            tag=f"{self.tag}_date_input_field",
+                            default_value=self.formatted_today_str,
+                            readonly=True, width=80)
+
+                    # Popup for Date    
+                    with dpg.popup(parent=f"{self.tag}_date_input_field", 
+                                    mousebutton=dpg.mvMouseButton_Left,
+                                    tag=f"{self.tag}_date_popup"):
+                        
+                        dpg.add_date_picker(level=dpg.mvDatePickerLevel_Day,
+                                            default_value=self.today_dict,
+                                            callback=self.date_picker_callback)
+                    dpg.set_item_pos(f"{self.tag}_date_popup", [562, 141])
+
+                    # Study Arm
+                    with dpg.group(horizontal=True):
+                        dpg.add_text("Study Arm: ")
+                        dpg.add_spacer(width=10)
+
+                        dpg.add_checkbox(label="PBM Only")
+                        dpg.add_spacer(width=10)
+
+                        dpg.add_checkbox(label="PBM + Music Therapy")
+                        dpg.add_spacer(width=10)
+
+                        dpg.add_checkbox(label="PBM + Baduanjin")
+                        dpg.add_spacer(width=10)
+
+                        dpg.add_checkbox(label="Control")
+
+                    dpg.add_spacer(height=5)
+
+                    # SECTION A: SOCIODEMOGRAPHIC INFORMATION
+                    with dpg.collapsing_header(label="SECTION A: SOCIODEMOGRAPHIC INFORMATION", 
+                                               closable=False, default_open=True):
+                        dpg.add_spacer(height=3)
+                        
+                        with dpg.table(header_row=True, borders_innerH=True, borders_outerH=True, 
+                            borders_innerV=True, borders_outerV=True, width=1000):
+                    
+                            # Add the columns with label
+                            dpg.add_table_column(label="Data Item")
+                            dpg.add_table_column(label="Response")
+
+                            # Add the row contents
+                            ## Row 1
+                            with dpg.table_row():
+                                dpg.add_text("Age")
+                                dpg.add_input_text(width=-1)
+
+                            ## Row 2
+                            with dpg.table_row():
+                                dpg.add_text("Sex")
+                                dpg.add_input_text(width=-1)
+
+                            ## Row 3
+                            with dpg.table_row():
+                                dpg.add_text("Civil Status")
+                                dpg.add_input_text(width=-1)
+
+                            ## Row 4
+                            with dpg.table_row():
+                                dpg.add_text("Educational Attainment")
+                                dpg.add_input_text(width=-1)
+
+                            ## Row 5
+                            with dpg.table_row():
+                                dpg.add_text("Occupation")
+                                dpg.add_input_text(width=-1)
+
+                            ## Row 6
+                            with dpg.table_row():
+                                dpg.add_text("Living Arrangement")
+                                dpg.add_input_text(width=-1)
+
+                            ## Row 7
+                            with dpg.table_row():
+                                dpg.add_text("Primary Caregive (Name/Relationship)")
+                                dpg.add_input_text(width=-1)
+
+                            ## Row 8
+                            with dpg.table_row():
+                                dpg.add_text("Mobile Number of Caregiver")
+                                dpg.add_input_text(width=-1)
+
+                    dpg.add_spacer(height=15)
+
+                    # SECTION B: CLINICAL HISTORY AND ELIGIBILITY
+                    with dpg.collapsing_header(label="SECTION B: CLINICAL HISTORY AND ELIGIBILITY", 
+                                            closable=False, default_open=True):
+                        dpg.add_spacer(height=3)
+                        
+                        with dpg.table(header_row=True, borders_innerH=True, borders_outerH=True, 
+                            borders_innerV=True, borders_outerV=True, width=1000):
+                    
+                            # Add the columns with label
+                            dpg.add_table_column(label="Item")
+                            dpg.add_table_column(label="Yes / No / N/A")
+                            dpg.add_table_column(label="Remarks")
+
+                            # Add the row contents
+                            ## Row 1
+                            with dpg.table_row(height=10):
+                                dpg.add_text("Diagnosed with MCI or early-stage dementia?")
+                                dpg.add_input_text(width=-1, multiline=True)
+                                dpg.add_input_text(width=-1, multiline=True)
+
+                            ## Row 2
+                            with dpg.table_row(height=10):
+                                dpg.add_text("History of stroke/TBI/epilepsy")
+                                dpg.add_input_text(width=-1, multiline=True)
+                                dpg.add_input_text(width=-1, multiline=True)
+
+                            ## Row 3
+                            with dpg.table_row(height=10):
+                                dpg.add_text("Visual or auditory impairment")
+                                dpg.add_input_text(width=-1, multiline=True)
+                                dpg.add_input_text(width=-1, multiline=True)
+
+                            ## Row 4
+                            with dpg.table_row(height=10):
+                                dpg.add_text("Using pacemaker or implanted devices")
+                                dpg.add_input_text(width=-1, multiline=True)
+                                dpg.add_input_text(width=-1, multiline=True)
+
+                            ## Row 5
+                            with dpg.table_row(height=10):
+                                dpg.add_text("Taking psychoactive  medications")
+                                dpg.add_input_text(width=-1, multiline=True)
+                                dpg.add_input_text(width=-1, multiline=True)
+
+                    dpg.add_spacer(height=15)
+
+                    # SECTION C: INTERVENTION MONITORING (Per Session)
+                    with dpg.collapsing_header(label="SECTION C: INTERVENTION MONITORING (Per Session)", 
+                                            closable=False, default_open=True):
+                        dpg.add_spacer(height=3)
+                        
+                        with dpg.table(header_row=True, borders_innerH=True, borders_outerH=True, 
+                            borders_innerV=True, borders_outerV=True, width=1000):
+                    
+                            # Add the columns with label
+                            dpg.add_table_column(label="Session No.")
+                            dpg.add_table_column(label="Date")
+                            dpg.add_table_column(label="Intervention\nCompleted")
+                            dpg.add_table_column(label="Notes on \nBehavior / Reaction")
+                            dpg.add_table_column(label="Adverse Event (Y/N)")
+                            dpg.add_table_column(label="Remarks")
+
+                            # Add the row contents
+                            ## Row 1
+                            with dpg.table_row(height=10):
+                                dpg.add_text("1")
+                                dpg.add_input_text(width=-1, multiline=True)
+                                dpg.add_input_text(width=-1, multiline=True)
+                                dpg.add_input_text(width=-1, multiline=True)
+                                dpg.add_input_text(width=-1, multiline=True)
+                                dpg.add_input_text(width=-1, multiline=True)
+
+                            ## Row 2
+                            with dpg.table_row(height=10):
+                                dpg.add_text("2")
+                                dpg.add_input_text(width=-1, multiline=True)
+                                dpg.add_input_text(width=-1, multiline=True)
+                                dpg.add_input_text(width=-1, multiline=True)
+                                dpg.add_input_text(width=-1, multiline=True)
+                                dpg.add_input_text(width=-1, multiline=True)
+
+                            ## Row 3
+                            with dpg.table_row(height=10):
+                                dpg.add_text("3")
+                                dpg.add_input_text(width=-1, multiline=True)
+                                dpg.add_input_text(width=-1, multiline=True)
+                                dpg.add_input_text(width=-1, multiline=True)
+                                dpg.add_input_text(width=-1, multiline=True)
+                                dpg.add_input_text(width=-1, multiline=True)
+
+                            ## Row 4
+                            with dpg.table_row(height=10):
+                                dpg.add_text("4")
+                                dpg.add_input_text(width=-1, multiline=True)
+                                dpg.add_input_text(width=-1, multiline=True)
+                                dpg.add_input_text(width=-1, multiline=True)
+                                dpg.add_input_text(width=-1, multiline=True)
+                                dpg.add_input_text(width=-1, multiline=True)
+
+                            ## Row 5
+                            with dpg.table_row(height=10):
+                                dpg.add_text("5")
+                                dpg.add_input_text(width=-1, multiline=True)
+                                dpg.add_input_text(width=-1, multiline=True)
+                                dpg.add_input_text(width=-1, multiline=True)
+                                dpg.add_input_text(width=-1, multiline=True)
+                                dpg.add_input_text(width=-1, multiline=True)
+
+                    dpg.add_spacer(height=15)
+                    
+                    # SECTION D: POST-INTERVENTION ASSESSMENTS
+                    with dpg.collapsing_header(label="SECTION D: POST-INTERVENTION ASSESSMENTS", 
+                                            closable=False, default_open=True):
+                        dpg.add_spacer(height=3)
+                        
+                        with dpg.table(header_row=True, borders_innerH=True, borders_outerH=True, 
+                            borders_innerV=True, borders_outerV=True, width=1000):
+                    
+                            # Add the columns with label
+                            dpg.add_table_column(label="Assessmen Tool")
+                            dpg.add_table_column(label="Raw Score")
+                            dpg.add_table_column(label="Percentile / Interpretation")
+
+                            # Add the row contents
+                            ## Row 1
+                            with dpg.table_row(height=10):
+                                dpg.add_text("MMSE")
+                                dpg.add_input_text(width=-1)
+                                dpg.add_input_text(width=-1)
+
+                            ## Row 2
+                            with dpg.table_row(height=10):
+                                dpg.add_text("MoCA")
+                                dpg.add_input_text(width=-1)
+                                dpg.add_input_text(width=-1)
+
+                            ## Row 3
+                            with dpg.table_row(height=10):
+                                dpg.add_text("GDS")
+                                dpg.add_input_text(width=-1)
+                                dpg.add_input_text(width=-1)
+
+                            ## Row 4
+                            with dpg.table_row(height=10):
+                                dpg.add_text("ADL Score")
+                                dpg.add_input_text(width=-1)
+                                dpg.add_input_text(width=-1)
+
+                            ## Row 5
+                            with dpg.table_row(height=10):
+                                dpg.add_text("NPI-Q")
+                                dpg.add_input_text(width=-1)
+                                dpg.add_input_text(width=-1)
+
+                            ## Row 6
+                            with dpg.table_row(height=10):
+                                dpg.add_text("EEG Changes (Band Power, Coherence, etc.)")
+                                dpg.add_input_text(width=-1)
+                                dpg.add_input_text(width=-1)
+
+                            ## Row 7
+                            with dpg.table_row(height=10):
+                                dpg.add_text("ERP")
+                                dpg.add_input_text(width=-1)
+                                dpg.add_input_text(width=-1)
+
+                            ## Row 8
+                            with dpg.table_row(height=10):
+                                dpg.add_text("HRV")
+                                dpg.add_input_text(width=-1)
+                                dpg.add_input_text(width=-1)
+
+                            ## Row 9
+                            with dpg.table_row(height=10):
+                                dpg.add_text("Skin Temperature (mean °C)")
+                                dpg.add_input_text(width=-1)
+                                dpg.add_input_text(width=-1)
+
+                    dpg.add_spacer(height=15)
+                                    
+                    # SECTION E: QUALITATIVE FEEDBACK (Participant and/or Caregiver)
+                    with dpg.collapsing_header(label="SECTION E: QUALITATIVE FEEDBACK (Participant and/or Caregiver)", 
+                                            closable=False, default_open=True):
+                        dpg.add_spacer(height=3)
+
+                        # Question 1
+                        dpg.add_text("1. How did you find the experience with the device and the interventions?")
+                        dpg.add_input_text(width=1000, height=60, multiline=True)
+
+                        # Question 2
+                        dpg.add_text("2. Did you notice any improvements in your daily life?")
+                        dpg.add_input_text(width=1000, height=60, multiline=True)
+
+                        # Question 3
+                        dpg.add_text("3. Were there any discomforts or concerns?")
+                        dpg.add_input_text(width=1000, height=60, multiline=True)
+
+                        # Question 4
+                        dpg.add_text("4. Suggestions for improvement?")
+                        dpg.add_input_text(width=1000, height=60, multiline=True)
+
+
+    def _get_date_today(self):
+        now = datetime.now()
+
+        # GUI String Format
+        self.formatted_today_str = now.strftime("%m/%d/%Y")
+
+        # DPG Date Picker String Format
+        self.today_dict = {
+            'month_day': now.day,
+            'month': now.month - 1,     # DPG months are 0-11
+            'year': now.year - 1900     # DPG years are counted since 1900
+        }
+
+    ### Callbacks
+    def date_picker_callback(self, sender, app_data, user_data):
+        if app_data is not None:
+            year = app_data['year'] + 1900
+            month = app_data['month'] + 1
+            day = app_data['month_day']
+            
+            formatted_date = f"{month:02d}/{day:02d}/{year}"
+            
+            dpg.set_value(f"{self.tag}_date_input_field", formatted_date)
+        
+        dpg.configure_item(f"{self.tag}_date_popup", show=False)
+
 
 
 class EEGTab:
